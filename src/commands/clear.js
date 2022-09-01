c873b59777db393117780e5920f9e258fbcb54c5
AlessandroTurnover <94559274+AlessandroTurnover@users.noreply.github.com>
2022-09-01 17:28:11 -0300

    feat: add clear function

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,10 +1,33 @@
-module.exports = {
-    name: "clear",
-    description: "Para limpar mensagens em um chat",
-    category: "staff",
-    timeout: 3000,
-    aliases: ["limpar"],
+const { EmbedBuilder, Colors } = require('discord.js');
 
-    async execute(client, message, args) {
+module.exports = {
+  name: 'clear',
+  description: 'Para limpar mensagens em um chat',
+  category: 'staff',
+  timeout: 3000,
+  aliases: ['limpar'],
+
+  async execute(client, message, args) {
+    try {
+      if (!message.member.permissions.has('Administrator')) return;
+      const amount = args[0] || 100;
+      if (!amount || isNaN(amount) || parseInt(amount) < 1) return;
+      message.delete();
+      const embedAuthor = new EmbedBuilder()
+        .setColor(Colors.DarkButNotBlack)
+        .setDescription(
+          '```diff\n- [APP] MESSAGE DELETE ERROR: SÓ É POSSÍVEL DELETAR MENSAGENS COM MENOS DE 14 DIAS QUE FORAM ENVIADAS\n```'
+        )
+        .setTimestamp();
+      message.channel.bulkDelete(amount, true).catch((e) => {
+        console.log(
+          '[APP] MESSAGE DELETE ERROR: SÓ É POSSÍVEL DELETAR MENSAGENS COM MENOS DE 14 DIAS QUE FORAM ENVIADAS'
+        );
+        return message.author.send({ embeds: [embedAuthor] });
+      });
+      return;
+    } catch (e) {
+      return console.log(e);
     }
-}
+  },
+};
